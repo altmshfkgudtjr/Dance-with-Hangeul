bb4b752740a70a542708d90d5ff76efb17e543a6
wo7864 <wo78644@gmail.com>
2021-05-19 00:05:14 +0900

    210511 feedback

--- src/modules/wordAnime/templates/canvas/ting.js
+++ src/modules/wordAnime/templates/canvas/ting.js
@@ -1,50 +1,56 @@
-import HangulCanvas from '../HangulCanvas';
-import Ting from '../hangul/ting';
+class Canvas_Ting extends HangulCanvas {
 
-export default class Canvas_Ting extends HangulCanvas {
-  constructor(props) {
-    super(props);
-    this.HangulClass = Ting;
-    this.maxObjectCount = 1;
-    this.maxCooltime = 120;
-  }
-  init() {
-    super.init();
-    this.ctx.globalAlpha = 1;
-    this.ctx.fillStyle = '#fff';
-  }
+    constructor(props) {
+        super(props);
+        this.HangulClass = Ting;
+        this.maxObjectCount = 1;
+        this.maxCooltime = 120;
 
-  start() {
-    //const colors = ["#ffc107", "#80bdff", "#ff9800"]
-    const step = timestamp => {
-      this.createObject();
-      this.detectObjects();
-      this.removeObject();
-      this.drawBackground();
-      this.drawObject();
-      this.animation = requestAnimationFrame(step);
-    };
-    this.animation = requestAnimationFrame(step);
-  }
+    }
+    init() {
+        super.init();
+        this.ctx.globalAlpha = 1
+        this.ctx.fillStyle = "#fff";
+    }
 
-  getParams() {
-    const groupId = new Date();
-    const fontSize = 100;
-    const x = 500;
-    const superParams = super.getParams();
-    const params = {
-      ...superParams,
-      groupId: groupId,
-      fontSize: fontSize,
-      fontFamily: 'Nanum Myeongjo',
-      text: '펄럭',
-      opacity: 1,
-      x: x,
-      y: 500,
-      life: 800,
-      isGravity: false,
-    };
+    start() {
 
-    return params;
-  }
+        //const colors = ["#ffc107", "#80bdff", "#ff9800"]
+        const step = (timestamp) => {
+
+
+            this.createObject();
+            this.detectObjects();
+            this.removeObject();
+            this.drawBackground();
+            this.drawObject();
+            this.animation = requestAnimationFrame(step);
+
+        }
+        this.animation = requestAnimationFrame(step);
+    }
+
+    getParams() {
+        const groupId = new Date();
+        const fontSize = 100
+        const x = 500
+        const superParams = super.getParams();
+        const params = {
+            ...superParams,
+            groupId: groupId,
+            fontSize: fontSize,
+            fontFamily: 'Nanum Myeongjo',
+            text: '펄럭',
+            opacity: 1,
+            x: x,
+            y: 500,
+            life: 800,
+            isGravity: false,
+        }
+       
+        return params
+    }
+
+
+
 }
